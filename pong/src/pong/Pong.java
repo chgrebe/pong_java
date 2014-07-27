d2093bf863a105d4fb0fe4bc19683065f735fbca
Christian Grebe <christian.grebe@gmx.net>
2014-07-28 00:40:12 +0200

    Naive implementation of game ticks. Can produce increasing lags if the game updates are slow. Lags while rendering are just lags as expected.

--- pong/src/pong/Pong.java
+++ pong/src/pong/Pong.java
@@ -1,6 +1,5 @@
 package pong;
 
-
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
@@ -18,36 +17,30 @@ import pong.mouse_control.MouseControl;
 
 public class Pong implements Runnable {
 
-	private static Pong me = null;
+	private static double accumulatorUpdate = 0.0;
+	private static BufferStrategy bufferStrategy;
+	private static Canvas canvas;
+	private static double currentRender;
+	private static double updateCurrent;
+	private static double deltaRender;
+	private static double updateDelta;
+	private static JFrame frame;
+	private static double lastRender = System.currentTimeMillis();
+	private static double lastUpdate = System.currentTimeMillis();
+	private static boolean notFinished = true;
+	private static JPanel panel;
+	private static Pong me = new Pong();
 
 	public static Pong get() {
-		if (me == null) {
-			synchronized (Pong.class) {
-				if (me == null) {
-					me = new Pong();
-				}
-			}
-		}
 		return me;
 	}
-
-	public JPanel panel;
-	public Canvas canvas;
-	public JFrame frame;
 
 	// TESTING
 	private float x, y = 0;
 
-	BufferStrategy bufferStrategy;
-
-	long desiredGameDeltaLoop = (1000 * 1000 * 1000) / Const.GAME_DESIRED_FPS.intValue();
-
-	boolean running = true;
-
 	private Pong() {
 		frame = new JFrame("Basic Pong");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// frame.pack();
 		frame.setSize(new Dimension(Const.GUI_WIDTH.intValue(), Const.GUI_HEIGHT.intValue()));
 		frame.setResizable(false);
 		frame.setVisible(true);
@@ -101,39 +94,47 @@ public class Pong implements Runnable {
 	@Override
 	public void run() {
 
-		long beginLoopTime;
-		long endLoopTime;
-		long currentUpdateTime = System.nanoTime();
-		long lastUpdateTime;
-		long deltaLoop;
+		final double desiredGUIDelta = (1000) / Const.GUI_DESIRED_FPS.doubleValue();
+		final double desiredGameDelta = (1000) / Const.GAME_DESIRED_FPS.doubleValue();
+		System.out.println("Game Delta: " + desiredGameDelta);
+		System.out.println("GUI Delta: " + desiredGUIDelta);
 
-		while (running) {
-			beginLoopTime = System.nanoTime();
+		while (notFinished) {
 
-			render();
+			updateCurrent = System.currentTimeMillis();
+			updateDelta = updateCurrent - lastUpdate;
+			lastUpdate = updateCurrent;
+			accumulatorUpdate += updateDelta;
 
-			lastUpdateTime = currentUpdateTime;
-			currentUpdateTime = System.nanoTime();
-//			int deltaTime = (int) (currentUpdateTime - lastUpdateTime);
-//			TODO: hier weitermachen
-//			update(ticksToUpdate);
-			System.out.print((currentUpdateTime - lastUpdateTime) + ", ");
-
-			endLoopTime = System.nanoTime();
-			deltaLoop = endLoopTime - beginLoopTime;
-			System.out.println(deltaLoop + ", " + desiredGameDeltaLoop);
-
-			if (deltaLoop > desiredGameDeltaLoop) {
-				// Do nothing. We are already late.
-			} else {
+			// System.out.println(accumulator);
+			while (accumulatorUpdate >= desiredGameDelta) {
+				System.out.printf("Time since last update: %f%n", Double.valueOf(updateDelta));
+				update(desiredGameDelta);
+				accumulatorUpdate -= desiredGameDelta;
 				try {
-					System.out.println((desiredGameDeltaLoop - deltaLoop) / (1000 * 1000));
-					Thread.sleep((desiredGameDeltaLoop - deltaLoop) / (1000 * 1000));
-				} catch (final InterruptedException e) {
-					// Do nothing
+					Thread.sleep(0);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			currentRender = System.currentTimeMillis();
+			deltaRender = currentRender - lastRender;
+
+			if (deltaRender >= desiredGUIDelta) {
+				System.out.println("Time since last render: " + deltaRender);
+				render();
+				lastRender = currentRender;
+				try {
+					Thread.sleep(0);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 			}
 		}
+
 	}
 
 	private void render() {
@@ -154,22 +155,13 @@ public class Pong implements Runnable {
 	/**
 	 * Rewrite this method for your game
 	 */
-	protected void update(final double ticksToUpdate) {
-		
-		if (ticksToUpdate > 1) {
-			System.out.println("_______________________________________________________________________________");
-		}
-		// simulated lag
-		try {
-			Thread.sleep(0);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+	protected void update(final double dt) {
 
-		System.out.println(ticksToUpdate);
-		x += ticksToUpdate;
-		if (y > Const.GUI_HEIGHT.intValue() - 200) {
-			// x = 0;
+		x += dt * 0.1;
+		y += dt * 0.1;
+
+		if (x > Const.GUI_WIDTH.intValue() - 200) {
+			x = 0;
 			y = 0;
 		}
 	}
--- pong/src/pong/constants/Const.java
+++ pong/src/pong/constants/Const.java
@@ -5,6 +5,6 @@ public class Const {
 	public static final Integer GUI_HEIGHT = Integer.valueOf(768);
 	public static final Integer GUI_WIDTH = Integer.valueOf(1024);
 	public static final Integer GUI_DESIRED_FPS = Integer.valueOf(60);
-	public static final Integer GAME_DESIRED_FPS = Integer.valueOf(50);
+	public static final Integer GAME_DESIRED_FPS = Integer.valueOf(100);
 
 }
